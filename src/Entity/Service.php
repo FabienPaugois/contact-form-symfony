315accfdb33f81214816f5d74fbe69fd291ae523
Fabien Paugois <fabien.paugois@viacesi.fr>
2021-09-07 15:40:37 +0200

    Added API support

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -6,9 +6,12 @@ use App\Repository\ServiceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
 
 /**
  * @ORM\Entity(repositoryClass=ServiceRepository::class)
+ * @ApiResource
  */
 class Service
 {
